6f7e52829b85449be10f18b0a95f93190e0698fc
Neto Deolino <netodeolino@gmail.com>
2017-06-16 17:56:25 -0300

    Refatoração

--- TCC 01/Extração/extratorpy.py
+++ TCC 01/Extração/extratorpy.py
@@ -2,11 +2,17 @@
 import numpy as np
 import pandas
 
-# - Global (global nomeVariavel)
+
+# - Variáveis globais (global nomeVariavel)
+ENTIDADE_NAO_ENCOTRADA = -1
+ENTIDADE_VAZIA = " NÃO POSSUI ESSA INFORMAÇÃO"
+
+
+# - Valores para agilizar no processo de ler vários .CSV's por vez
 mes = "jan"
 listDiasMes = ["01", "02", "03", "04", "05", "06", "07", "08", "10", "12", "14", "15", "16", "17", "19", "20",
 				"21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"]
-#tes = None
+
 tes = pandas.read_csv("./tabula-07-" + mes + "-17.csv")
 lista = []
 arrayLinha = []
@@ -20,9 +26,6 @@ del tes["FONTE"]
 del tes["NATUREZA DA OCORRÊNCIA"]
 
 
-#for i in tes.iterrows():
-#	print (i)
-
 # - Salva em 'lista' as linhas de HISTÓRICO DA OCORRÊNCIA
 for i, row in tes.iterrows():
 	if row['HISTÓRICO DA OCORRÊNCIA']:
@@ -33,18 +36,13 @@ for k in lista:
 	#print (k.values)
 	arrayLinha.append(k.values)
 
-# - Verificando quantidade de linhas
-#print(len(arrayLinha))
-#print(type(arrayLinha[0]))
-
-#for l in arrayLinha[0]:
-#	print (l)
 
 # - Remover da stringLinha os dados que foram extraídos dela?
 def excluirTextoDaString(string, inicio, fim):
 	pass
 
 
+# - arrayLinha[posicao] é do tipo narra, por isso, para facilitar, é feito a conversão para string
 def transformaArrayEmString(lista):
 	aux = ""
 	for strCelula in lista:
@@ -72,12 +70,11 @@ def extrair(string, inicio, entidade):
 	print (entidade + " " + aux)
 
 
-# Recebe a string completa de uma linha do HISTÓRICO DA OCORRÊNCIA
+# - Recebe a string completa de uma linha do HISTÓRICO DA OCORRÊNCIA e extrai a partir do primeiro ':'
+# - Essa função sempre começa lendo do início da string 'stringLinha', portanto só pega o primeiro ':' e '.'
 def extrairEntrePontos(string):
 	i = 0;
 	while i < len(string):
-		# - Se encontrar dois pontos chamar 'extrair()' passando a string e a posição que deve começar a extrair
-		# - Essa função sempre começa lendo do início da string 'stringLinha', portanto só pega o primeiro ':' e '.'
 		if (string[i] == ":"):
 			extrair(string, i+2) #Pegar o tamanho dos espaços
 			break
@@ -96,6 +93,9 @@ def tirarEspacosDoInicio(string, inicio):
 
 # - Extrai uma entidade, porém tem que tratar o caso de quando a entidade não existe
 def extrairUmaEntidade(arrayLinha, entidade):
+	global ENTIDADE_VAZIA
+	global ENTIDADE_NAO_ENCOTRADA
+
 	t = 0
 	while t < len(arrayLinha):
 		stringLinha = transformaArrayEmString(arrayLinha[t])
@@ -103,8 +103,9 @@ def extrairUmaEntidade(arrayLinha, entidade):
 
 		posicaoIni = stringLinha.find(entidade)
 
-		if (posicaoIni == -1):
-			print (entidade + " NÃO POSSUI ESSA INFORMAÇÃO")
+		# - Se não existe a entidade o valor retornada pelo find é -1
+		if (posicaoIni == ENTIDADE_NAO_ENCOTRADA):
+			print (entidade + ENTIDADE_VAZIA)
 		else:
 			posicaoIni = posicaoIni + len(entidade)
 		
@@ -113,20 +114,24 @@ def extrairUmaEntidade(arrayLinha, entidade):
 			extrair(stringLinha, posicaoIni, entidade)
 		t += 1
 
+
 # - Extrai uma entidade, porém tem que tratar o caso de quando a entidade não existe
 def extrairUmaEntidadeEmUmaLinha(arrayLinha, entidade, posicao):
+	global ENTIDADE_VAZIA
+	global ENTIDADE_NAO_ENCOTRADA
+
 	# - arrayLinha[posicao] é do tipo narray, e anteriormente list, por isso, para facilitar, é feito a conversão para string
 	stringLinha = transformaArrayEmString(arrayLinha[posicao])
 	print (stringLinha + "\n")
 
 	posicaoIni = stringLinha.find(entidade)
 
-	if (posicaoIni == -1):
-		print (entidade + " NÃO POSSUI ESSA INFORMAÇÃO")
+	if (posicaoIni == ENTIDADE_NAO_ENCOTRADA):
+		print (entidade + ENTIDADE_VAZIA)
 	else:
 		posicaoIni = posicaoIni + len(entidade)
 	
-		# - Tira os espaços desnecessários do início da string
+		# - Tira os espaços desnecessários do início da string antes dela ser extraída
 		posicaoIni = tirarEspacosDoInicio(stringLinha, posicaoIni)
 
 		extrair(stringLinha, posicaoIni, entidade)
@@ -134,6 +139,9 @@ def extrairUmaEntidadeEmUmaLinha(arrayLinha, entidade, posicao):
 
 # - Extrai todas entidades, porém tem que tratar o caso de quando a entidade não existe
 def extrairTodasEntidades(arrayLinha):
+	global ENTIDADE_VAZIA
+	global ENTIDADE_NAO_ENCOTRADA
+
 	t = 0
 	while t < len(arrayLinha):
 		# - arrayLinha[t] é do tipo narray, e anteriormente list, por isso, para facilitar, é feito a conversão para string
@@ -143,8 +151,8 @@ def extrairTodasEntidades(arrayLinha):
 		for entidade in entidades:
 			posicaoIni = stringLinha.find(entidade)
 
-			if (posicaoIni == -1):
-				print (entidade + " NÃO POSSUI ESSA INFORMAÇÃO")
+			if (posicaoIni == ENTIDADE_NAO_ENCOTRADA):
+				print (entidade + ENTIDADE_VAZIA)
 			else:
 				posicaoIni = posicaoIni + len(entidade)
 
@@ -154,6 +162,18 @@ def extrairTodasEntidades(arrayLinha):
 				extrair(stringLinha, posicaoIni, entidade)
 		print ("\n")
 		t += 1
+
+
+# - Verificando quantidade de linhas
+#print(len(arrayLinha))
+#print(type(arrayLinha[0]))
+
+#for l in arrayLinha[0]:
+#	print (l)
+
+
+#for i in tes.iterrows():
+#	print (i)
 
 
 #extrairEntrePontos(stringLinha)
