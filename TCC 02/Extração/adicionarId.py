31a2dbfb9ea322243d493f3a85890854aeff2f54
Neto Deolino <netodeolino@gmail.com>
2017-08-24 13:57:54 -0300

    criar coluna ID e preencher

---
+++ TCC 02/Extração/adicionarId.py
@@ -0,0 +1,24 @@
+# -*- coding: UTF-8 -*-
+import numpy as np
+import pandas
+
+arrayDeFrames = []
+
+
+def main():
+	# Ler arquivo e salva no frame
+	dataFrame = pandas.read_csv("./janeiro-crimes.csv")
+	
+	# Cria o indice e preenche (ID)
+	dataFrame['ID'] = range(1, len(dataFrame) + 1)
+
+	# Reestrutura as colunas
+	dataFrame = dataFrame[['ID', 'FONTE', 'NATUREZA DA OCORRÊNCIA', 'HISTÓRICO DA OCORRÊNCIA', 'LOCAL:', 'SUSPEITO:',
+				'VEÍCULO:', 'VÍTIMA:', 'VÍTIMAS:', 'VÍTIMA FATAL:', 'ARMA APREENDIDA:', 'MATERIAL APREENDIDO:',
+				'PLACA:', 'VÍTIMAS LESIONADAS:', 'SUSPEITOS:', 'DATA', 'LONGITUDE', 'LATITUDE']]
+
+	# Salva o novo arquivo CSV
+	dataFrame.to_csv("./huashuas.csv", index=False)
+
+if __name__ == '__main__':
+	main()
