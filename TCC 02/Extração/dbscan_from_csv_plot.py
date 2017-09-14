057f00f1c0de017a54dc38f1791b8c16b8cd0877
Neto Deolino <netodeolino@gmail.com>
2017-09-13 22:09:27 -0300

    Dbscan salvando nova coluna de id

--- TCC 02/Extração/dbscan_from_csv_plot.py
+++ TCC 02/Extração/dbscan_from_csv_plot.py
@@ -16,15 +16,12 @@ from sklearn.cluster import DBSCAN
 from sklearn import metrics
 from sklearn.datasets.samples_generator import make_blobs
 from sklearn.preprocessing import StandardScaler
-
+from collections import defaultdict
 
 # #############################################################################
-# Generate sample data
-#centers = [[1, 1], [-1, -1], [1, -1]]
-#X, labels_true = make_blobs(n_samples=750, centers=centers, cluster_std=0.4,
-#                            random_state=0)
-
+# Read the file CSV and save in frame
 dataFrame = pandas.read_csv("./janeiro-crimes.csv")
+novoFrame = dataFrame.copy()
 
 X = []
 
@@ -33,36 +30,35 @@ for i, row in dataFrame.iterrows():
     X.append([float(row['LATITUDE']), float(row['LONGITUDE'])])
     #print (row['LONGITUDE'], row['LATITUDE'])
 
-X = StandardScaler().fit_transform(X)
-
-print ('--------------------------------------------')
-print (X)
-print ('--------------------------------------------')
+#X = StandardScaler().fit_transform(X)
+X = np.asmatrix(X)
 
 # #############################################################################
+# define the number of kilometers in one radian
+kms_per_radian = 6371.0088
+
+# define epsilon and min_samples; converted epsilon to radians for use by haversine
+epsilon = 1.1 / kms_per_radian
+minimo_samples = 11
+
 # Compute DBSCAN
-db = DBSCAN(eps=0.3, min_samples=3, metric='euclidean').fit(X)
+db = DBSCAN(eps=epsilon, min_samples=minimo_samples, metric='haversine').fit(np.radians(X))
 core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
 core_samples_mask[db.core_sample_indices_] = True
 labels = db.labels_
 
-print ('--------------------------------------------')
-print (labels)
-print ('--------------------------------------------')
+listaFlor = []
+for a in labels:
+	listaFlor.append(a)
+	#print (a)
+
+# #############################################################################
+# Save the cluster_id in new column in CSV file
+novoFrame["CLUSTER"] = pandas.Series(listaFlor)
+novoFrame.to_csv("./flor-janeiro.csv", index=False)
 
 # Number of clusters in labels, ignoring noise if present.
 n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-
-#print('Estimated number of clusters: %d' % n_clusters_)
-#print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
-#print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
-#print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
-#print("Adjusted Rand Index: %0.3f"
-#      % metrics.adjusted_rand_score(labels_true, labels))
-#print("Adjusted Mutual Information: %0.3f"
-#      % metrics.adjusted_mutual_info_score(labels_true, labels))
-#print("Silhouette Coefficient: %0.3f"
-#      % metrics.silhouette_score(X, labels))
 
 # #############################################################################
 # Plot result
@@ -70,20 +66,24 @@ import matplotlib.pyplot as plt
 
 # Black removed and is used for noise instead.
 unique_labels = set(labels)
+
 colors = [plt.cm.Spectral(each)
           for each in np.linspace(0, 1, len(unique_labels))]
+
 for k, col in zip(unique_labels, colors):
     if k == -1:
         # Black used for noise.
         col = [0, 0, 0, 1]
 
     class_member_mask = (labels == k)
-
+    
     xy = X[class_member_mask & core_samples_mask]
+
     plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
              markeredgecolor='k', markersize=14)
 
     xy = X[class_member_mask & ~core_samples_mask]
+    
     plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
              markeredgecolor='k', markersize=6)
 
